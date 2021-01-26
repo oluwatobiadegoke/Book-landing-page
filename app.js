cd628b872c30f75563755a46e33971cfce539e88
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-01-26 13:47:22 +0100

    Further update

--- app.js
+++ app.js
@@ -15,3 +15,24 @@ undrop.addEventListener('click', function() {
     drop.style.opacity = "1";
     logo.style.opacity = "1";
 })
+
+// Tabs selectors
+
+const buttons = document.querySelectorAll(".tab-btn");
+const thetab = document.querySelector(".thetab");
+const contents = document.querySelectorAll(".s-two");
+
+thetab.addEventListener("click", function(e) {
+    const id = e.target.dataset.id;
+    if(id) {
+        buttons.forEach(function(btn) {
+            btn.classList.remove("active");
+            e.target.classList.add("active")
+        });
+        contents.forEach(function(content) {
+            content.classList.remove("active")
+        })
+        const x = document.getElementById(id);
+        x.classList.add("active")
+    }
+})
